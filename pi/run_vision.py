617716b677d96c36e2a23310cea5d3e34825c255
Clayton Ramsey <claytonwramsey@gmail.com>
2023-01-23 10:53:00 -0600

    implement columnar depth vision

---
+++ pi/run_vision.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+
+import cv2
+import depthai as dai
+
+NUM_COLUMNS = 10
+
+# Create pipeline
+pipeline = dai.Pipeline()
+
+# Define sources and outputs
+monoLeft = pipeline.create(dai.node.MonoCamera)
+monoRight = pipeline.create(dai.node.MonoCamera)
+stereo = pipeline.create(dai.node.StereoDepth)
+spatialLocationCalculator = pipeline.create(dai.node.SpatialLocationCalculator)
+
+xoutDepth = pipeline.create(dai.node.XLinkOut)
+xoutSpatialData = pipeline.create(dai.node.XLinkOut)
+xinSpatialCalcConfig = pipeline.create(dai.node.XLinkIn)
+
+xoutDepth.setStreamName("depth")
+xoutSpatialData.setStreamName("spatialData")
+xinSpatialCalcConfig.setStreamName("spatialCalcConfig")
+
+# Properties
+monoLeft.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
+monoLeft.setBoardSocket(dai.CameraBoardSocket.LEFT)
+monoRight.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
+monoRight.setBoardSocket(dai.CameraBoardSocket.RIGHT)
+
+lrcheck = False
+subpixel = False
+
+stereo.setDefaultProfilePreset(dai.node.StereoDepth.PresetMode.HIGH_DENSITY)
+stereo.setLeftRightCheck(lrcheck)
+stereo.setSubpixel(subpixel)
+
+spatialLocationCalculator.inputConfig.setWaitForMessage(False)
+
+# Linking
+monoLeft.out.link(stereo.left)
+monoRight.out.link(stereo.right)
+
+spatialLocationCalculator.passthroughDepth.link(xoutDepth.input)
+stereo.depth.link(spatialLocationCalculator.inputDepth)
+
+spatialLocationCalculator.out.link(xoutSpatialData.input)
+xinSpatialCalcConfig.out.link(spatialLocationCalculator.inputConfig)
+
+for col in range(NUM_COLUMNS):
+    # Set up column configuration
+    topLeft = dai.Point2f(col / NUM_COLUMNS, 0.0)
+    bottomRight = dai.Point2f((col + 1) / (NUM_COLUMNS), 1.0)
+
+    cfgdata = dai.SpatialLocationCalculatorConfigData()
+    cfgdata.depthThresholds.lowerThreshold = 100
+    cfgdata.depthThresholds.upperThreshold = 10000
+    cfgdata.roi = dai.Rect(topLeft, bottomRight)
+    spatialLocationCalculator.initialConfig.addROI(cfgdata)
+
+# Connect to device and start pipeline
+with dai.Device(pipeline) as device:
+
+    # Output queue will be used to get the depth frames from the outputs defined above
+    depthQueue = device.getOutputQueue(name="depth", maxSize=4, blocking=False)
+    spatialCalcQueue = device.getOutputQueue(name="spatialData", maxSize=4, blocking=False)
+    spatialCalcConfigInQueue = device.getInputQueue("spatialCalcConfig")
+
+    color = (255, 255, 255)
+
+    while True:
+            
+        inDepth = depthQueue.get() # Blocking call, will wait until a new data has arrived
+
+        depthFrame = inDepth.getFrame() # depthFrame values are in millimeters
+
+        spatialData = spatialCalcQueue.get().getSpatialLocations()
+        
+        depths = [int(data.spatialCoordinates.z) for data in spatialData]
+        print(depths)
