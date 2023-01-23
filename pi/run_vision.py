d8a63a4f0d1fb040d86131b5f46b2eaf40f93735
Clayton Ramsey <claytonwramsey@gmail.com>
2023-01-23 11:04:01 -0600

    starting breaking things

--- pi/run_vision.py
+++ pi/run_vision.py
@@ -2,6 +2,19 @@
 
 import cv2
 import depthai as dai
+import argparse
+
+labelMap = ["background", "aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow",
+            "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
+
+nnPathDefault = str((Path(__file__).parent / Path('../models/mobilenet-ssd_openvino_2021.4_5shave.blob')).resolve().absolute())
+parser = argparse.ArgumentParser()
+parser.add_argument('nnPath', nargs='?', help="Path to mobilenet detection network blob", default=nnPathDefault)
+parser.add_argument('-ff', '--full_frame', action="store_true", help="Perform tracking on full RGB frame", default=False)
+
+args = parser.parse_args()
+
+fullFrameTracking = args.full_frame
 
 NUM_COLUMNS = 10
 
@@ -14,13 +27,24 @@ monoRight = pipeline.create(dai.node.MonoCamera)
 stereo = pipeline.create(dai.node.StereoDepth)
 spatialLocationCalculator = pipeline.create(dai.node.SpatialLocationCalculator)
 
+
+camRgb = pipeline.create(dai.node.ColorCamera)
+spatialDetectionNetwork = pipeline.create(dai.node.MobileNetSpatialDetectionNetwork)
+objectTracker = pipeline.create(dai.node.ObjectTracker)
+
 xoutDepth = pipeline.create(dai.node.XLinkOut)
 xoutSpatialData = pipeline.create(dai.node.XLinkOut)
 xinSpatialCalcConfig = pipeline.create(dai.node.XLinkIn)
 
+xoutRgb = pipeline.create(dai.node.XLinkOut)
+trackerOut = pipeline.create(dai.node.XLinkOut)
+
 xoutDepth.setStreamName("depth")
 xoutSpatialData.setStreamName("spatialData")
 xinSpatialCalcConfig.setStreamName("spatialCalcConfig")
+
+xoutRgb.setStreamName("preview")
+trackerOut.setStreamName("tracklets")
 
 # Properties
 monoLeft.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
@@ -28,12 +52,20 @@ monoLeft.setBoardSocket(dai.CameraBoardSocket.LEFT)
 monoRight.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
 monoRight.setBoardSocket(dai.CameraBoardSocket.RIGHT)
 
+camRgb.setPreviewSize(300, 300)
+camRgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
+camRgb.setInterleaved(False)
+camRgb.setColorOrder(dai.ColorCameraProperties.ColorOrder.BGR)
+
 lrcheck = False
 subpixel = False
 
 stereo.setDefaultProfilePreset(dai.node.StereoDepth.PresetMode.HIGH_DENSITY)
 stereo.setLeftRightCheck(lrcheck)
 stereo.setSubpixel(subpixel)
+# Align depth map to the perspective of RGB camera, on which inference is done
+stereo.setDepthAlign(dai.CameraBoardSocket.RGB)
+stereo.setOutputSize(monoLeft.getResolutionWidth(), monoLeft.getResolutionHeight())
 
 spatialLocationCalculator.inputConfig.setWaitForMessage(False)
 
