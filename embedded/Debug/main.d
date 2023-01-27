531b44f71090f0a31dce117d6f3cc2438720f427
Max Aksel Bowman <maxabowman@gmail.com>
2023-01-27 16:41:24 -0600

    Add proof of concept embedded software and project skeleton.

---
+++ embedded/Debug/main.d
@@ -0,0 +1,60 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/Program\ Files/ccs/ccs_base/msp430/include/msp430.h
+main.obj: C:/Program\ Files/ccs/ccs_base/msp430/include/msp430g2553.h
+main.obj: C:/Program\ Files/ccs/ccs_base/msp430/include/in430.h
+main.obj: C:/Program\ Files/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/intrinsics.h
+main.obj: C:/Program\ Files/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/intrinsics_legacy_undefs.h
+main.obj: C:/Program\ Files/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/stdlib.h
+main.obj: C:/Program\ Files/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/_ti_config.h
+main.obj: C:/Program\ Files/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/linkage.h
+main.obj: C:/Program\ Files/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/sys/cdefs.h
+main.obj: C:/Program\ Files/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/stdio.h
+main.obj: C:/Program\ Files/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/stdarg.h
+main.obj: C:/Program\ Files/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/sys/_types.h
+main.obj: C:/Program\ Files/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/machine/_types.h
+main.obj: C:/Program\ Files/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/string.h
+main.obj: C:/Program\ Files/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/xlocale/_string.h
+main.obj: C:/Program\ Files/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/math.h
+main.obj: C:/Program\ Files/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/_defs.h
+main.obj: C:/Program\ Files/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/machine/_limits.h
+
+../main.c:
+
+C:/Program\ Files/ccs/ccs_base/msp430/include/msp430.h:
+
+C:/Program\ Files/ccs/ccs_base/msp430/include/msp430g2553.h:
+
+C:/Program\ Files/ccs/ccs_base/msp430/include/in430.h:
+
+C:/Program\ Files/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/intrinsics.h:
+
+C:/Program\ Files/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/intrinsics_legacy_undefs.h:
+
+C:/Program\ Files/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/stdlib.h:
+
+C:/Program\ Files/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/_ti_config.h:
+
+C:/Program\ Files/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/linkage.h:
+
+C:/Program\ Files/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/sys/cdefs.h:
+
+C:/Program\ Files/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/stdio.h:
+
+C:/Program\ Files/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/stdarg.h:
+
+C:/Program\ Files/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/sys/_types.h:
+
+C:/Program\ Files/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/machine/_types.h:
+
+C:/Program\ Files/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/string.h:
+
+C:/Program\ Files/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/xlocale/_string.h:
+
+C:/Program\ Files/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/math.h:
+
+C:/Program\ Files/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/_defs.h:
+
+C:/Program\ Files/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/machine/_limits.h:
+
