6af68316c2844a0bc61917fa883b71f124e4b405
Clayton Ramsey <claytonwramsey@gmail.com>
2023-01-27 10:38:35 -0600

    get object tracking running but it stops occasionally ???

--- pi/run_vision.py
+++ pi/run_vision.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from pathlib import Path
 import cv2
 import depthai as dai
 import argparse
@@ -7,7 +8,7 @@ import argparse
 labelMap = ["background", "aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow",
             "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
 
-nnPathDefault = str((Path(__file__).parent / Path('../models/mobilenet-ssd_openvino_2021.4_5shave.blob')).resolve().absolute())
+nnPathDefault = str((Path(__file__).parent / Path('mobilenet-ssd_openvino_2021.4_5shave.blob')).resolve().absolute())
 parser = argparse.ArgumentParser()
 parser.add_argument('nnPath', nargs='?', help="Path to mobilenet detection network blob", default=nnPathDefault)
 parser.add_argument('-ff', '--full_frame', action="store_true", help="Perform tracking on full RGB frame", default=False)
@@ -57,7 +58,7 @@ camRgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
 camRgb.setInterleaved(False)
 camRgb.setColorOrder(dai.ColorCameraProperties.ColorOrder.BGR)
 
-lrcheck = False
+lrcheck = True
 subpixel = False
 
 stereo.setDefaultProfilePreset(dai.node.StereoDepth.PresetMode.HIGH_DENSITY)
@@ -69,9 +70,39 @@ stereo.setOutputSize(monoLeft.getResolutionWidth(), monoLeft.getResolutionHeight
 
 spatialLocationCalculator.inputConfig.setWaitForMessage(False)
 
+spatialDetectionNetwork.setBlobPath(args.nnPath)
+spatialDetectionNetwork.setConfidenceThreshold(0.5)
+spatialDetectionNetwork.input.setBlocking(False)
+spatialDetectionNetwork.setBoundingBoxScaleFactor(0.5)
+spatialDetectionNetwork.setDepthLowerThreshold(100)
+spatialDetectionNetwork.setDepthUpperThreshold(5000)
+
+objectTracker.setDetectionLabelsToTrack([15])  # track only person
+# possible tracking types: ZERO_TERM_COLOR_HISTOGRAM, ZERO_TERM_IMAGELESS, SHORT_TERM_IMAGELESS, SHORT_TERM_KCF
+objectTracker.setTrackerType(dai.TrackerType.ZERO_TERM_COLOR_HISTOGRAM)
+# take the smallest ID when new object is tracked, possible options: SMALLEST_ID, UNIQUE_ID
+objectTracker.setTrackerIdAssignmentPolicy(dai.TrackerIdAssignmentPolicy.SMALLEST_ID)
+
 # Linking
 monoLeft.out.link(stereo.left)
 monoRight.out.link(stereo.right)
+
+camRgb.preview.link(spatialDetectionNetwork.input)
+objectTracker.passthroughTrackerFrame.link(xoutRgb.input)
+objectTracker.out.link(trackerOut.input)
+
+if fullFrameTracking:
+    camRgb.setPreviewKeepAspectRatio(False)
+    camRgb.video.link(objectTracker.inputTrackerFrame)
+    objectTracker.inputTrackerFrame.setBlocking(False)
+    # do not block the pipeline if it's too slow on full frame
+    objectTracker.inputTrackerFrame.setQueueSize(2)
+else:
+    spatialDetectionNetwork.passthrough.link(objectTracker.inputTrackerFrame)
+
+spatialDetectionNetwork.passthrough.link(objectTracker.inputDetectionFrame)
+spatialDetectionNetwork.out.link(objectTracker.inputDetections)
+stereo.depth.link(spatialDetectionNetwork.inputDepth)
 
 spatialLocationCalculator.passthroughDepth.link(xoutDepth.input)
 stereo.depth.link(spatialLocationCalculator.inputDepth)
@@ -97,16 +128,26 @@ with dai.Device(pipeline) as device:
     depthQueue = device.getOutputQueue(name="depth", maxSize=4, blocking=False)
     spatialCalcQueue = device.getOutputQueue(name="spatialData", maxSize=4, blocking=False)
     spatialCalcConfigInQueue = device.getInputQueue("spatialCalcConfig")
+    
+    # things that are being tracked
+    tracklets = device.getOutputQueue("tracklets", 4, False)
 
     color = (255, 255, 255)
 
     while True:
-            
         inDepth = depthQueue.get() # Blocking call, will wait until a new data has arrived
-
+        print("wait for depth frame")
         depthFrame = inDepth.getFrame() # depthFrame values are in millimeters
 
+        print("wait for spatial data")
         spatialData = spatialCalcQueue.get().getSpatialLocations()
+        print("wait for tracklets")
+        trackletsData = tracklets.get().tracklets
+        print("make depths")
         
         depths = [int(data.spatialCoordinates.z) for data in spatialData]
         print(depths)
+        print("display tracklet data")
+        for t in trackletsData:
+            label = labelMap[t.label] if t.label < len(labelMap) else str(t.label)
+            print(f"found tracklet {t.id}: {label} at ({t.spatialCoordinates.x}, {t.spatialCoordinates.y}, {t.spatialCoordinates.z})") 
