d68a9b031c439a498cdb3a056b6389ae2e6eae1a
Clayton Ramsey <claytonwramsey@gmail.com>
2023-02-02 18:52:00 -0600

    notes on running main

---
+++ pi/README.md
@@ -0,0 +1,10 @@
+# Raspberry Pi code for HeadsUp
+
+## Usage
+
+To run the Raspberry Pi system, execute the following command in the same folder containing
+`main.py`:
+
+```sh
+python main.py
+```
--- pi/main.py
+++ pi/main.py
@@ -3,12 +3,18 @@ from vision import VisionSystem
 
 import depthai as dai
 
-pipeline = dai.Pipeline()
 
-orientation = Orientation(pipeline)
-vision = VisionSystem(pipeline)
+def main():
+    pipeline = dai.Pipeline()
 
-with dai.Device(pipeline) as device:
-    vision.periodic()
-    orientation.periodic()
-    print(orientation.current_quaternion())
+    orientation = Orientation(pipeline)
+    vision = VisionSystem(pipeline)
+
+    with dai.Device(pipeline) as device:
+        vision.periodic()
+        orientation.periodic()
+        print(orientation.current_quaternion())
+
+
+if __name__ == "__main__":
+    main()
