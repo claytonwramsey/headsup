88eae5d3e48ce59b6c6e7693f6e7891722ec7b7e
Clayton Ramsey <claytonwramsey@gmail.com>
2023-02-13 21:40:41 -0600

    create demo for rising edges on audio system

--- audio/src/main.rs
+++ audio/src/main.rs
@@ -1,3 +1,48 @@
-fn main() {
-    println!("Hello, world!");
+use std::time::Instant;
+
+use gpio_cdev::{EventRequestFlags, LineRequestFlags};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    /// The GPIO pin IDs which are associated with microphone input.
+    const MIC_INPUT_PINS: [u32; 8] = [0, 1, 2, 3, 4, 5, 6, 7];
+
+    let mut chip = gpio_cdev::Chip::new("/dev/gpiochip0")?;
+    let event_sources = MIC_INPUT_PINS.iter().map(|&pin| {
+        chip.get_line(pin)
+            .unwrap()
+            .events(
+                LineRequestFlags::INPUT,
+                EventRequestFlags::RISING_EDGE,
+                "headsup-audio",
+            )
+            .unwrap()
+    });
+
+    let start_time = Instant::now();
+
+    std::thread::scope(|s| {
+        let mut handles = Vec::new();
+        for (mic_id, event_source) in event_sources.enumerate() {
+            handles.push(s.spawn(move || {
+                for event in event_source {
+                    match event {
+                        Ok(_) => {
+                            println!(
+                                "rising edge on mic thread {mic_id} at time {:?}",
+                                Instant::now().duration_since(start_time)
+                            )
+                        }
+                        Err(_) => println!("error on mic thread {mic_id}"),
+                    }
+                }
+            }));
+        }
+
+        for handle in handles {
+            // wait for all threads to die (this will never happen)
+            handle.join().unwrap();
+        }
+    });
+
+    Ok(())
 }
