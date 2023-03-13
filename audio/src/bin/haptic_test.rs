3b9d4bc658ba7f0e98bfd32fe5e68e95b07de0ac
Max Bowman <maxabowman@gmail.com>
2023-03-13 11:50:14 -0500

    Fix haptics test code.

--- audio/src/bin/haptic_test.rs
+++ audio/src/bin/haptic_test.rs
@@ -2,7 +2,7 @@
 fn main() {
     println!("Running haptics test! Remember to make sure the GPIO pin assignment in `haptics.rs` is correct!");
     loop {
-        for id in 0..8 {
+        for id in 0..1 {
             println!("Vibrating buzzer {id}...");
             headsup_audio::haptics::buzz(id);
         }
