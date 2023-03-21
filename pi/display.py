76451fd5a8c56c8a4a41dae546654edce32983c6
Max Bowman <maxabowman@gmail.com>
2023-03-21 17:15:16 -0500

    Fix radius type

--- pi/display.py
+++ pi/display.py
@@ -53,7 +53,7 @@ class Display:
 
         # Draw distance circles
         for circle_ind in range(1, int(self.MAX_DEPTH / self.RING_DELTA)):
-            output_img = cv2.circle(output_img, (self.size // 2, 0), self.RING_DELTA*float(circle_ind), (255, 255, 255), 2)
+            output_img = cv2.circle(output_img, (self.size // 2, 0), int(self.RING_DELTA*float(circle_ind)), (255, 255, 255), 2)
 
         # Draw FOV lines
         output_img = cv2.line(output_img, (0, self.size/(2*np.tan(self.FOV//2))), (self.size//2, 0), (255, 255, 255), 2)
