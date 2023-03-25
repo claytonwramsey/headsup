69f36949d4a505685f3a7130aedca8815b21656d
Max Bowman <maxabowman@gmail.com>
2023-03-24 20:47:47 -0500

    UI updates and Altium files.

--- pi/display.py
+++ pi/display.py
@@ -17,7 +17,8 @@ class Display:
     """
     MIN_DEPTH = 1*12*25.4  # in mm
     MAX_DEPTH = 20*12*25.4  # in mm
-    RING_DELTA = 5*12*25.4  # in mm
+    RING_DELTA_FT = 5
+    RING_DELTA = RING_DELTA_FT*12*25.4  # in mm
     FOV = 70  # degrees
 
     def __init__(self, motion_color: Tuple[float, float, float],
@@ -35,6 +36,10 @@ class Display:
         self.static_color = static_color
         self.size = size
         self.icon_size = icon_size
+        self.font = cv2.FONT_HERSHEY_SIMPLEX
+        self.font_scale = 1
+        self.font_thickness = 1
+        self.line_type = 2
 
     def update_radar_screen(self, img: np.array, rt_pairs: List[Tuple[float, float]]) -> np.array:
         """
@@ -50,13 +55,27 @@ class Display:
         # Draw distance circles
         for circle_ind in range(1, int(self.MAX_DEPTH / self.RING_DELTA)):
             radar_img = cv2.circle(radar_img, (self.size // 2, 0), int(self.RING_DELTA*float(circle_ind)*self.size/self.MAX_DEPTH), (255, 255, 255), 1)
+            cv2.putText(radar_img, f"{circle_ind*self.RING_DELTA_FT}",
+                        (self.size//2, int(self.RING_DELTA*float(circle_ind)*self.size/self.MAX_DEPTH)), self.font, self.font_scale, (255, 255, 255),
+                        self.font_thickness, self.line_type)
 
         # Draw FOV lines
         radar_img = cv2.line(radar_img, (0, int(self.size/(2*np.tan(self.FOV//2)))), (self.size//2, 0), (255, 255, 255), 1)
         radar_img = cv2.line(radar_img, (self.size//2, 0), (self.size, int(self.size/(2*np.tan(self.FOV//2)))), (255, 255, 255), 1)
 
+        # Add scale
+        cv2.putText(radar_img, "ft.",
+                    (0, self.size), self.font,
+                    self.font_scale, (255, 255, 255),
+                    self.font_thickness, self.line_type)
+
         # Draw user circle
-        radar_img = cv2.circle(radar_img, (self.size//2, 0), self.icon_size, (255, 0, 0), -1)
+        pt1 = (self.size//2, 0)
+        pt2 = (self.size//2 - self.icon_size, self.icon_size*2)
+        pt3 = (self.size//2 + self.icon_size, self.icon_size*2)
+        triangle_points = np.array([pt1, pt2, pt3])
+        radar_img = cv2.drawContours(radar_img, [triangle_points], 0, (0, 0, 255), -1)
+        # radar_img = cv2.circle(radar_img, (self.size//2, 0), self.icon_size, (255, 0, 0), -1)
 
         for pair in rt_pairs:
             r, theta = pair
